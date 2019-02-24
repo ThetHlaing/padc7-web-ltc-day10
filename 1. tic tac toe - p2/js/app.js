3311df565ffb8830d926958385d206d413290fdd
thethlaing-mmdsgit config --global user.name thethlaing-mmds <thethlaing@mm-digital-solutions.com>
2019-02-24 15:52:48 +0630

    Initial

--- 1. tic tac toe - p2/js/app.js
+++ 1. tic tac toe - p2/js/app.js
@@ -1,14 +1,24 @@
+import Player from '/js/model/Player.js';
+
 const gameModel = {
-    player1_name : "My Name",
-    player2_name : "Your Name",
-    player1_positions : [],
-    player2_positions : [],
-    wining_positions : [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9]
+    current_player_index : 1,
+
+    get currentPlayer(){
+        if(this.current_player_index === 1){
+            return this.player1;
+        }else{
+            return this.player2;
+        }
+    },
+
+    player1: {},
+    player2: {},
+    wining_positions: [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
     ]
-    
+
 };
 
 const gameController = {
@@ -17,10 +27,13 @@ const gameController = {
      * init function for the main controller
      * object creation will happen here
      */
-    init(){
-       //const result =  this.isGameEnd([4],gameModel.wining_positions);
-       //console.log(result);
-       gameView.init();
+    init() {
+        //const result =  this.isGameEnd([4],gameModeStevenl.wining_positions);
+        //console.log(result);
+        gameModel.player1 = new Player("Ryan", 'O');
+        gameModel.player2 = new Player("Steven", 'X');
+
+        gameView.init();
     },
 
     /**
@@ -29,43 +42,63 @@ const gameController = {
      * @param {Array} positions 
      * @param {Array} wining_positions 
      */
-    isGameEnd(positions,wining_positions){
+    isGameEnd(positions, wining_positions) {
 
         //looping over positions 
-        for(const index in wining_positions){
+        for (const index in wining_positions) {
             let is_game_end = true;
 
             //looping over item
-            for(const j in wining_positions[index]){
+            for (const j in wining_positions[index]) {
                 const current_wining_position = wining_positions[index][j];
 
                 //if not matching item is found
                 //End the search
-                if(positions.indexOf(current_wining_position) === -1){
+                if (positions.indexOf(current_wining_position) = -1) {
                     is_game_end = false;
                 }
             }
-            if(is_game_end){
+            if (is_game_end) {
                 console.log("the game is end");
                 return true;
             }
         }
         return false;
+    },
+
+    addPosition(position){
+        //current player's position store
+
+        console.log(gameModel.currentPlayer);
+
+        //check if the game is end
+
+        //change the current user
+
+        //re-render
     }
-
-
 };
 
 const gameView = {
-    
+    init() {
+        //Select all checkbox
+        this.checkboxes = document.querySelectorAll(".game-view input[type='checkbox']");
+        for (const box of this.checkboxes) {
+            box.addEventListener('change', (event) => this.checkboxChange(event));
+        }
+    },
+    checkboxChange(event) {
+        const current_block = event.target.dataset.block;
+        gameController.addPosition(current_block);
+    }
 }
 
 const registerView = {
-    
+
 }
 
 const scoreBoardView = {
-    
+
 }
 
 gameController.init();
